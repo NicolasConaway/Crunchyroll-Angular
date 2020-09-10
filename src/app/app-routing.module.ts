10f349ab8ddd5ea2c7b5a6aaef67f17a75322d2c
Nicolas Conaway <n.conaway00@gmail.com>
2020-09-10 12:21:49 -0400

    fifth commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/pages/home/home.component';
+import { ShowsComponent } from './components/pages/shows/shows.component';
+import { MangaComponent } from './components/pages/manga/manga.component';
+import { NewsComponent } from './components/pages/news/news.component';
+import { GamesComponent } from './components/pages/games/games.component';
+import { StoreComponent } from './components/pages/store/store.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'shows', component: ShowsComponent },
+  { path: 'manga', component: MangaComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'games', component: GamesComponent },
+  { path: 'store', component: StoreComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ShowsComponent } from './components/pages/shows/shows.component';
+import { MangaComponent } from './components/pages/manga/manga.component';
+import { NewsComponent } from './components/pages/news/news.component';
+import { GamesComponent } from './components/pages/games/games.component';
+import { StoreComponent } from './components/pages/store/store.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    ShowsComponent,
+    MangaComponent,
+    NewsComponent,
+    GamesComponent,
+    StoreComponent
   ],
   imports: [
     BrowserModule,
